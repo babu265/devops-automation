77e87464333001376ce94217066e9fe3c53e7d28
Ramesh Dasari <dr.babu265@gmail.com>
2023-11-17 19:57:11 +0530

    counter file

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -1,21 +1,24 @@
 package com.javatechie;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
-@RestController
-public class DevopsIntegrationApplication {
+public class HelloWorldApp {
 
-	@GetMapping
-	public String message(){
-		return "welcome to javatechie";
-	}
+    private int counter = 0;
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsIntegrationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApp.class, args);
+    }
 
+    @GetMapping("/")
+    @ResponseBody
+    public String helloWorld() {
+        counter++;
+        return "Hello World - " + counter;
+    }
 }
